4dc8801e99cdbcf59388914b05514b8bb775a6fc
async1o <matvey.gusak2000@mail.ru>
2026-05-01 02:40:32 +0300

    fix cors

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from fastapi-stast-app!")
-
-
-if __name__ == "__main__":
-    main()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import asyncio
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from routers import router
 from db.db import create_db
@@ -10,6 +12,21 @@ from db.db import create_db
 
 app = FastAPI()
 app.include_router(router)
+
+origins = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 
 
 if __name__ == "__main__":
